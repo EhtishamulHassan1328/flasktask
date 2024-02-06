621ffddee6a2186706405cdcfd8f8f544cd4999b
EhtishamulHassan1328 <therealehtisham@gmail.com>
2024-02-06 13:52:11 +0500

    Created get Users info method which contains json object.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Created a virtual Environment and Activated it.
 Created Home route in my application.
+Created get Users info method which contains json object.
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app=Flask(__name__)
 
@@ -7,6 +7,21 @@ app=Flask(__name__)
 @app.route('/',methods=['GET'])
 def home():
     return "Home route"
+
+
+#Get users information route
+@app.route('/users',methods=['GET'])
+def getusersinfo():
+    user_info={
+        'users': [
+            {'name':'Ehtisham', 'age':18, 'gender':'Male'},
+            {'name':'Ali', 'age':19, 'gender':'Male'},
+            {'name':'Iqbal', 'age':25, 'gender':'Male'},
+            {'name':'Usman', 'age':28, 'gender':'Male'},
+            {'name':'Amna', 'age':40, 'gender':'Female'}
+        ]
+    }
+    return jsonify(user_info)
 
 
 if __name__=="__main__":
